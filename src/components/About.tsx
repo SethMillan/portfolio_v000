7fe97e285e24ce7dd365c1385e4cd8a7780c6cf1
Seth Millán <millanseth@gmail.com>
2025-09-16 08:16:18 -0600

    feat: first commit
    just run the tailwind

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+// AboutMe.tsx
+export default function AboutMe() {
+  return (
+    <section className="w-full max-w-3xl mx-auto p-6 bg-[#D9D9D9] rounded-2xl shadow-md">
+      <h2 className="text-2xl font-bold mb-4">Sobre mí</h2>
+      <p className="text-gray-800 leading-relaxed">
+        Probando tailwindcss con vite y react.
+      </p>
+    </section>
+  );
+}

2eb1945a571cb362419abfb28085e27ccac8b33d
Seth Millán <millanseth@gmail.com>
2025-09-17 08:39:24 -0600

    feat(About): :sparkles: added stats about me
    son los stats que tengo en el figma jeje quedaron chidillos

---
+++ src/components/About/Stats.tsx
@@ -0,0 +1,19 @@
+export const Stats = () => {
+  return (
+    <div className="pt-5 flex justify-between items-center max-w-2xl">  
+        <div className="text-center">
+            <p className="text-[var(--color-text)] font-[Roboto_Condensed] text-[38px] not-italic font-extrabold leading-normal">+15</p>
+            <p className="text-[var(--color-text)] font-[Roboto_Condensed] text-[24px] not-italic font-normal leading-normal">Projects Delivered</p>
+        </div> 
+        <div className="text-center">
+            <p className="text-[var(--color-text)] font-[Roboto_Condensed] text-[38px] not-italic font-extrabold leading-normal">+1000</p>
+            <p className="text-[var(--color-text)] font-[Roboto_Condensed] text-[24px] not-italic font-normal leading-normal">Users Impacted</p>
+        </div>
+        <div className=" text-center">
+            <p className="text-[var(--color-text)] font-[Roboto_Condensed] text-[38px] not-italic font-extrabold leading-normal">98%</p>
+            <p className="text-[var(--color-text)] font-[Roboto_Condensed] text-[24px] not-italic font-normal leading-normal">Clients Satisfaction</p>
+        </div>
+        
+    </div>
+  );
+}

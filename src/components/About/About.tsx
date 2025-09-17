b0a25d2f263615dcbade9f874201daaff5d80162
Seth Millán <millanseth@gmail.com>
2025-09-17 09:01:29 -0600

    feat(Service): :sparkles: background configured
    agregue el background del figma en svg

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,40 +6,75 @@ import { Stats } from "./Stats";
 // AboutMe.tsx
 export default function AboutMe() {
   return (
-    <section className="w-full h-[100vh] mx-auto p-6 flex">
+    <section className="w-full h-[120vh] mx-auto p-6 flex relative">
       <div className="pl-30">
         <p className="text-[var(--color-secondary)] font-[Roboto_Condensed] text-[42px] italic font-semibold leading-normal">
-        🚀 ABOUT ME
-      </p>
-      <p className="pt-1 text-[var(--color-text)] font-[Roboto_Condensed] text-[54px] not-italic font-extrabold leading-normal">
-        BEST PROJECT LEAD 
-      </p>
-      <p className="-mt-3 text-[var(--color-text)] font-[Roboto_Condensed] text-[54px] not-italic font-extrabold leading-normal">
-        & DEVELOPER IN LATAM 
-      </p>
-      
-      <p className="pt-1 text-[var(--color-text)] text-justify font-[Open_Sans] text-[28px] not-italic font-normal leading-[42px]">
-        My professional goal is to create solutions that drive growth <br/> and digital transformation within organizations.
-      </p>
+          🚀 ABOUT ME
+        </p>
+        <p className="pt-1 text-[var(--color-text)] font-[Roboto_Condensed] text-[54px] not-italic font-extrabold leading-normal">
+          BEST PROJECT LEAD
+        </p>
+        <p className="-mt-3 text-[var(--color-text)] font-[Roboto_Condensed] text-[54px] not-italic font-extrabold leading-normal">
+          & DEVELOPER IN LATAM
+        </p>
 
-      <Stats></Stats>
-      <div className="pt-12 flex justify-between items-center max-w-lg flex-col gap-6 sm:flex-row">
-        <BtnFilled color={"--color-primary"} letra={"28px"}>
-          DOWNLOAD CV
-          <img
-            src="/icons/download.svg"
-            alt="Download Icon"
-            className="inline-block ml-2 w-9 h-9"
-          />
-        </BtnFilled>
-        <BtnOutlined color={"--color-primary"} letra={"28px"}>
-          CONTACT ME !
-        </BtnOutlined>
-      </div>
+        <p className="pt-1 text-[var(--color-text)] text-justify font-[Open_Sans] text-[28px] not-italic font-normal leading-[42px]">
+          My professional goal is to create solutions that drive growth <br />{" "}
+          and digital transformation within organizations.
+        </p>
+
+        <Stats></Stats>
+        <div className="pt-12 flex justify-between items-center max-w-lg flex-col gap-6 sm:flex-row">
+          <BtnFilled color={"--color-primary"} letra={"28px"}>
+            DOWNLOAD CV
+            <img
+              src="/icons/download.svg"
+              alt="Download Icon"
+              className="inline-block ml-2 w-9 h-9"
+            />
+          </BtnFilled>
+          <BtnOutlined color={"--color-primary"} letra={"28px"}>
+            CONTACT ME !
+          </BtnOutlined>
+        </div>
       </div>
       <div className="flex flex-1 items-center justify-center -mt-25 ">
         <Nave></Nave>
       </div>
+      <svg
+        className="absolute left-0 -bottom-22 -z-1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="full"
+        height="381"
+        viewBox="0 0 1728 381"
+        fill="none"
+      >
+        <path
+          d="M1012.13 5.48145C1048.79 -4.7409 1077.59 3.19419 1102.58 21.0762C1127.61 38.9826 1148.82 66.8621 1170.24 96.5459C1191.63 126.204 1213.23 157.655 1238.99 182.57C1264.77 207.497 1294.76 225.92 1332.95 229.498C1377.21 233.643 1424.06 221.677 1469.41 201.434C1514.76 181.188 1558.65 152.648 1597.01 123.601C1635.37 94.5522 1668.21 64.9878 1691.46 42.6865C1703.08 31.5356 1712.31 22.1993 1718.63 15.6504C1721.79 12.376 1724.22 9.7986 1725.87 8.03906C1726.55 7.31049 1727.09 6.72064 1727.5 6.28125V380.5H0.5V60.4453C0.608638 60.5828 0.725583 60.7334 0.853516 60.8945C1.67558 61.9302 2.89385 63.45 4.47949 65.3877C7.651 69.2633 12.2934 74.8119 18.1768 81.5039C29.9429 94.8872 46.6775 112.849 66.5498 131.163C106.269 167.767 158.622 205.872 208.944 211.497C265.433 217.812 301.769 186.804 337.981 153.19C374.232 119.542 410.36 83.2918 466.543 78.498C505.919 75.1384 538.75 88.4272 568.208 109.468C597.681 130.52 623.743 159.305 649.589 186.913C675.413 214.499 701.028 240.916 729.56 257.106C758.124 273.315 789.62 279.28 827.167 265.972C886.25 245.03 902.503 187.994 919.011 132.331C935.553 76.5521 952.346 22.1576 1012.13 5.48145Z"
+          fill="#A3AED0"
+          stroke="#F5F7FA"
+        />
+      </svg>
+      
+      <svg
+        className="absolute left-0 -bottom-20 -z-2"
+        xmlns="http://www.w3.org/2000/svg"
+        width="full"
+        height="381"
+        viewBox="0 0 1728 381"
+        fill="none"
+      >
+        <path
+          d="M0 59C0 59 108.702 199.788 209 211C321.481 223.574 353.728 87.6221 466.5 78C624.613 64.5092 677.43 318.516 827 265.5C944.606 223.814 891.812 38.5226 1012 4.99998C1159.24 -36.0686 1180.8 214.744 1333 229C1509.56 245.538 1728 4.99998 1728 4.99998V381H0V59Z"
+          fill="#A3AED0"
+        />
+      </svg>
     </section>
   );
 }
+/* 
+* Aqui basicamente agregue como nuevo las dos olitas, las exporte de svg del figma que hice
+* y las puse en la parte de abajo del componente, una encima de la otra, la de arriba con un z-index menor para que se vea mas clara
+* y la de abajo con un z-index mayor para que se vea mas opaca, ademas de que le puse una clase absoluta para posicionarlas bien, 
+* de igual forma tuve que poner clase relativa la seccion padre para que las olas se posicionaran con respecto a esta
+*/
--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -3,8 +3,10 @@ import { Card } from "./Card";
 
 export const Service = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-10 bg-[var(--color-background)] py-20 px-5">
-      <h3 className="text-[#2E3A59] font-['Roboto_Condensed'] text-[48px] font-extrabold">Service</h3>
+    <div className="h-[110vh] flex flex-col items-center justify-center gap-10 bg-[var(--background-secondary)] py-20 px-5 relative">
+      <h3 className="text-[#2E3A59] font-['Roboto_Condensed'] text-[56px] font-extrabold">
+        Service
+      </h3>
       <div className="flex w-[80%] justify-center items-center gap-15">
         <Card
           title={"Software Development"}
@@ -20,8 +22,10 @@ export const Service = () => {
           icon={"./icons/code.svg"}
           color={"--color-primary"}
         />
-        <div className="rotate-12"><Nave></Nave></div>
-        
+        <div className="rotate-12">
+          <Nave></Nave>
+        </div>
+
         <Card
           title={"UI/UX Design"}
           description={
@@ -37,6 +41,38 @@ export const Service = () => {
           color={"--color-secondary"}
         />
       </div>
+      <svg
+        className="absolute left-0 -bottom-52 -z-1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="full"
+        height="280"
+        viewBox="0 0 1728 280"
+        fill="none"
+      >
+        <path
+          d="M1727.5 0.5V146.708C1727.48 146.718 1727.46 146.728 1727.45 146.738C1727.24 146.853 1726.94 147.022 1726.54 147.244C1725.74 147.688 1724.55 148.338 1723.02 149.167C1719.95 150.825 1715.48 153.196 1709.89 156.043C1698.69 161.737 1682.99 169.339 1664.94 176.967C1628.82 192.231 1583.4 207.572 1545.99 208C1495.94 208.573 1462.16 179.696 1428.3 152.022C1394.48 124.38 1360.58 97.9461 1310.44 104.004C1265.49 109.435 1239.99 139.301 1214.62 168.971C1189.23 198.676 1163.96 228.186 1119.44 233.504C1073.52 238.988 1041.45 215.447 1009.17 191.344C976.933 167.269 944.481 142.632 897.959 146.502C849.464 150.536 821.049 183.255 792.745 214.998C764.402 246.786 736.169 277.598 687.99 278.5C657 279.08 634.178 266.712 615.411 247.738C596.623 228.743 581.916 203.146 567.171 177.285C552.442 151.451 537.676 125.353 518.787 105.489C499.88 85.6062 476.821 71.9437 445.515 71C392.398 69.399 359.863 108.653 327.738 145.639C295.538 182.712 263.765 217.498 212.09 208.008C180.612 202.226 162.541 178.161 144.211 155.952C125.925 133.797 107.371 113.495 74.9619 116.002C59.0982 117.229 40.275 124.892 25.457 132.217C18.0395 135.883 11.609 139.472 7.0332 142.146C4.74517 143.482 2.92042 144.59 1.66699 145.364C1.19487 145.656 0.804217 145.901 0.5 146.092V0.5H1727.5Z"
+          fill="#A3AED0"
+          stroke="#F5F7FA"
+        />
+      </svg>
+      <svg
+        className="absolute left-0 -bottom-54 -z-2"
+        xmlns="http://www.w3.org/2000/svg"
+        width="full"
+        height="280"
+        viewBox="0 0 1728 280"
+        fill="none"
+      >
+        <path
+          d="M0 0H1728V147C1728 147 1621.02 207.641 1546 208.5C1445.47 209.652 1410.31 92.4406 1310.5 104.5C1221.03 115.31 1208.98 223.312 1119.5 234C1027.22 245.022 990.615 139.296 898 147C801.468 155.029 784.849 277.187 688 279C563.382 281.332 570.083 75.2556 445.5 71.5C339.824 68.3144 315.985 227.597 212 208.5C148.614 196.859 139.254 111.528 75 116.5C43.4756 118.939 0 147 0 147V0Z"
+          fill="#A3AED0"
+        />
+      </svg>
     </div>
   );
 };
+/*
+* Aqui tambien agregue los svg del figma para que se vieran hasta abajo, y de igual forma le puse posicion relativa a la seccion padre
+* para que las olas se posicionaran con respecto a esta, ademas de que le puse una clase absoluta a las olas para posicionarlas bien
+* y un z-index por que una tiene un bordeo claro y queria que se remarcara sobre la otra 
+*/

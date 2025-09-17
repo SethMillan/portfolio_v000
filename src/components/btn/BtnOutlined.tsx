f6a4f4b6f6098d6006248be36cc2b2f19f1838ba
Seth Millán <millanseth@gmail.com>
2025-09-16 18:27:37 -0600

    perf(About): :zap: unused variable
    xd

--- src/components/btn/BtnOutlined.tsx
+++ src/components/btn/BtnOutlined.tsx
@@ -7,7 +7,6 @@ type ButtonProps = {
 };
 
 export const BtnOutlined = ({ children, color, letra }: ButtonProps) => {
-  const textColor = `var(${color})`;
   const fontSize = letra;
     // ? Use una variable independiente pasandola por style para evitar problemas con tailwind
     const base = `border-[var(${color})] text-[var(${color})]  bg-transparent border-2 text-center font-[Roboto_Condensed] font-bold py-2 px-6 transition duration-200 hover:bg-[var(${color})] hover:text-white hover:scale-105 hover:shadow-lg cursor-pointer hover:scale-110 hover:shadow-lg`;
